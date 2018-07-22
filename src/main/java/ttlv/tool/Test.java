94987fb1a97201b7386be3dd3a8609e0d2e89329
mefashi <33420959+mefashi@users.noreply.github.com>
2018-07-22 18:16:50 +0800

    modify test code

--- src/main/java/ttlv/tool/Test.java
+++ src/main/java/ttlv/tool/Test.java
@@ -23,6 +23,20 @@ public class Test {
         ttlv2.values = msg2.getBytes();
         ttlv.childs.add(ttlv2);
 
+        TtlvTool.TTLV ttlv3 = new TtlvTool.TTLV();
+        ttlv3.tag = 0x1;
+        ttlv3.type = 0x1;
+        ttlv.childs.add(ttlv3);
+
+        TtlvTool.TTLV ttlv4 = new TtlvTool.TTLV();
+        ttlv4.tag = 0x1;
+        ttlv4.type = 0x0;
+        String msg4 = "hello3";
+        ttlv4.length = msg4.getBytes().length;
+        ttlv4.values = msg4.getBytes();
+
+        ttlv3.childs.add(ttlv4);
+
         TtlvTool ttlvTool = new TtlvTool();
         byte[] b = ttlvTool.pack(ttlv);
         for(byte b1 : b) {
@@ -39,28 +53,10 @@ public class Test {
             for(TtlvTool.TTLV ttlvfc : ttlvf.childs) {
                 System.out.println(ttlvfc.tag + " - " + ttlvfc.type + " - " + ttlvfc.length);
                 System.out.println(new String(ttlvfc.values));
-            }
-        }
-
-        System.out.println("-------------------------");
-        System.out.println("-------------------------");
-        System.out.println("-------------------------");
-
-        byte[] bpack = ttlvTool.pack(ttlv);
-        for(byte b1 : b) {
-            System.out.print(b1);
-        }
-        System.out.println();
-
-        TtlvTool.TTLV mTtlvUnpack = ttlvTool.unpack(b);
-        System.out.println(mTtlvUnpack.tag + " - " + mTtlvUnpack.type + " - " + mTtlvUnpack.length);
-        System.out.println("-------------------------");
-        for(TtlvTool.TTLV ttlvf : mTtlv.childs) {
-            System.out.println(ttlvf.tag + " - " + ttlvf.type + " - " + ttlvf.length);
-            System.out.println("-------------------------");
-            for(TtlvTool.TTLV ttlvfc : ttlvf.childs) {
-                System.out.println(ttlvfc.tag + " - " + ttlvfc.type + " - " + ttlvfc.length);
-                System.out.println(new String(ttlvfc.values));
+                for(TtlvTool.TTLV ttlvfc22 : ttlvfc.childs) {
+                    System.out.println(ttlvfc22.tag + " - " + ttlvfc22.type + " - " + ttlvfc22.length);
+                    System.out.println(new String(ttlvfc22.values));
+                }
             }
         }
     }
